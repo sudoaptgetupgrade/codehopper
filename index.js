aafb4ff0185c6fe5d0a20b28869639dbd74ea96c
sudoaptgetupgrade <40376723+sudoaptgetupgrade@users.noreply.github.com>
2020-06-24 22:42:47 +0200

    Commands tab added

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const TOKEN = process.env.TOKEN;
 
-client.login(TOKEN);
+//ENV constants
+const BOTTOKEN = process.env.BOTTOKEN;
+const GUILD_ID = process.env.GUILD_ID;
+
+//Commands
+const prefix = "!"
+const ownercmd = "server-owner"
+
+//Setup client for discord.js
+client.login(BOTTOKEN);
 
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`);
 });
 
+//General constants
+
+
 client.on('message', msg => {
-  if (msg.content === 'miss') {
-    msg.reply('Miss^^');
+  if (msg.content === prefix.concat(ownercmd)) {
+    msg.reply("Owner ID=".concat(msg.guild.ownerID));
   };
 });
